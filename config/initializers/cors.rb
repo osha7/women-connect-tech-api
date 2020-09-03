e4417bf736f60ff39cd23093c013ab2eaabc123b
osha7 <ogroetz@gmail.com>
2020-09-02 19:29:21 -0600

    cors allowance for PUT route

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -48,6 +48,15 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
+        origins 'http://localhost:3000/jobs/:id'
+
+        resource '*',
+            headers: :any,
+            methods: [:get, :post, :put, :patch, :delete, :options, :head],
+            credentials: true
+    end
+
+    allow do
         origins 'http://localhost:3002'
 
         resource '*',
