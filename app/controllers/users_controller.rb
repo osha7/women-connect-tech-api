58216a5f8ccf79b25231fc1a1556e690dbefb6b1
osha7 <ogroetz@gmail.com>
2020-09-03 12:34:47 -0600

    users_controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
             email: params['user']['email'],
             password: params['user']['password'],
             password_confirmation: params['user']['password_confirmation'],
-            # avatar: params['user']['avatar']
+            avatar: params['user']['avatar'],
+            website: params['user']['website']
             )
         if user 
             session[:user_id] = user.id
@@ -25,5 +26,26 @@ class UsersController < ApplicationController
             render json: { status: 500 }
         end
     end
+
+    def edit
+        user = User.find_by(id: params[:id])
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+        user.update(user_params)
+        if user.valid?
+            user.save
+            render json: user
+        else
+            render json: {error: 'Unable to save edits'}
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password_digest, :first_name, :last_name, :goals, :resume_link, :avatar, :website, :contact, :linked_in, :twitter, :open_to_connect, :open_to_mentor, :available_to_work, :saved)
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
-  resources :users, only: [:create, :index, :show]
+  resources :users, only: [:create, :index, :show, :edit, :update]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
