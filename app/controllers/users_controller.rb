41bafe1d95659637647a600e11a14e4aa8249bfb
osha7 <ogroetz@gmail.com>
2020-09-03 13:34:38 -0600

    users#show / cors update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
 
     def index
         users = User.all
@@ -7,8 +8,15 @@ class UsersController < ApplicationController
         }
     end
 
+    def show
+        # user = User.find_by(id: params[:id])
+        render json: {
+            user: @user
+        }
+    end
+
     def create
-        user = User.create!(
+        @user = User.create!(
             username: params['user']['username'],
             email: params['user']['email'],
             password: params['user']['password'],
@@ -16,11 +24,11 @@ class UsersController < ApplicationController
             avatar: params['user']['avatar'],
             website: params['user']['website']
             )
-        if user 
-            session[:user_id] = user.id
+        if @user 
+            session[:user_id] = @user.id
             render json: {
                 status: :created,
-                user: user
+                user: @user
             }
         else
             render json: { status: 500 }
@@ -28,15 +36,13 @@ class UsersController < ApplicationController
     end
 
     def edit
-        user = User.find_by(id: params[:id])
     end
 
     def update
-        user = User.find_by(id: params[:id])
-        user.update(user_params)
-        if user.valid?
-            user.save
-            render json: user
+        @user.update(user_params)
+        if @user.valid?
+            @user.save
+            render json: @user
         else
             render json: {error: 'Unable to save edits'}
         end
@@ -47,5 +53,10 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:email, :password_digest, :first_name, :last_name, :goals, :resume_link, :avatar, :website, :contact, :linked_in, :twitter, :open_to_connect, :open_to_mentor, :available_to_work, :saved)
+    end
+
+    def set_user
+        @user = User.find_by(id: params[:id])
+    end
 
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -30,6 +30,15 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
+        origins 'http://localhost:3000/users/:id'
+
+        resource '*',
+            headers: :any,
+            methods: [:get, :post, :put, :patch, :delete, :options, :head],
+            credentials: true
+    end
+
+    allow do
         origins 'http://localhost:3000/avatars'
 
         resource '*',
