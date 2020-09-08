fedc67a9ee7fa2689c94dcbd27cbc78afe4b5ee1
osha7 <ogroetz@gmail.com>
2020-09-08 14:00:25 -0600

    fix to user authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
-        if user
+        # byebug
+        if user && user.username == params["user"]["username"]
             session[:user_id] = user.id
             render json: {
                 status: :created,
